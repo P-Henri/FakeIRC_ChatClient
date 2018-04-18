fe1e483498c07a6da0b129de7171172956067e32
Patrick Henri <patrickhenri_@outlook.com>
2018-04-18 04:04:47 -0400

    minor changes

--- app.js
+++ app.js
@@ -10,16 +10,26 @@ let server = http.createServer(app)
 const io = require('socket.io')(server, {wsEngine: 'ws'})
 const uuidv4 = require('uuid/v4')
 
-app.use(express.static('public'))
+
+
+// Enable reverse proxy support in Express. This causes the
+// the "X-Forwarded-Proto" header field to be trusted so its
+// value can be used to determine the protocol. See
+// http://expressjs.com/api#app-settings for more details.
 app.enable('trust proxy');
+// Add a handler to inspect the req.secure flag (see
+// http://expressjs.com/api#req.secure). This allows us
+// to know whether the request was via http or https.
 app.use(function (req, res, next) {
     if (req.secure) {
+        // request was via https, so do no special handling
         next();
-    }
-    else {
+    } else {
+        // request was via http, so redirect to https
         res.redirect('https://' + req.headers.host + req.url);
     }
-})
+});
+app.use(express.static('public'))
 
 let colors = [
     "#ef9a9a",
